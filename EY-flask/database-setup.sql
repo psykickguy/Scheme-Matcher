8736035d86b4ea6926e4e74346c4a25307cac5b5
aniruddha <121780935+blueboi77@users.noreply.github.com>
2025-01-03 16:12:23 +0530

    updated app.py, website still very broken

--- EY-flask/database-setup.sql
+++ EY-flask/database-setup.sql
@@ -3,7 +3,8 @@ CREATE DATABASE IF NOT EXISTS EY;
 USE EY;
 
 CREATE TABLE users (
-    aadhar numeric(12) not null unique PRIMARY KEY,
+    uid INT AUTO_INCREMENT PRIMARY KEY,
+    aadhaar numeric(12) not null unique,
     email VARCHAR(50) NOT NULL UNIQUE,
     mobile numeric(10) not null UNIQUE,
     name text(50) not null
